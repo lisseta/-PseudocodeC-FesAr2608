7cb873cd45352add6fb02c315a128ab087030819
Norukz <dnoru.colin@gmail.com>
2022-05-11 23:37:09 -0500

    Update Compilador de pseudoc.py

--- Compilador de pseudoc.py
+++ Compilador de pseudoc.py
@@ -22,41 +22,46 @@ def esSimboloEsp(c):
     return c in especiales
 
 
+def esPalReservada(cad):
+    reservadas = ["main", "char", "int", "float", "double", "string", "if", "else", "do", "while", "for", "switch",
+                  "short", "long", "extern", "static", "default", "continue", "break", "register", "sizeof", "typedef"]
+    return cad in reservadas
 
-def quitaComentarios(cad):   # Quita Comentarios correcto
+def esTipo(cad):
+    tipos = ["int", "char", "float", "double","string"]
+    return cad in tipos
 
-    
- estado = "Z"
 
- # Aqui se recibe el codigo para quitar comentarios
- cadComentado = "main(){ /*inicio*/ \n int a;  /*se declara a*/"  #Eliminar esta cuando ya reciba  el codigo
+def esId(cad):
+    return (cad[0] in "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
- cadSinComentar = ""
 
- for c in cadComentado:
-    if (estado == "Z"):
-        if (c == "/"):
-            estado = "A"
-        else:
-            cadSinComentar = cadSinComentar + c
-    elif (estado == "A"):
-        if (c == "*"):
-            estado = "B"
-        else:
-            estado = "Z"
-            cadSinComentar = cadSinComentar+"/"+c
-    elif (estado == "B"):
-        if (c == "*"):
-            estado = "C"
-    elif(estado == "C"):
-        if (c == "/"):
-            estado = "Z"
-        else:
-            estado = "B"
-            
-    #print(cadSinComentar)
-
- return cadSinComentar
+def quitaComentarios(cad):
+    # estados: A, B, C, Z
+    estado = "Z"    
+    #cad = "a=b/c;"
+    cad2 = ""
+    for c in cad:
+        if (estado == "Z"):
+            if (c == "/"):
+                estado = "A"
+            else:
+                cad2 = cad2 + c
+        elif (estado == "A"):
+            if (c == "*"):
+                estado = "B"
+            else:
+                estado = "Z"
+                cad2 = cad2+"/"+c
+        elif (estado == "B"):
+            if (c == "*"):
+                estado = "C"
+        elif(estado == "C"):
+            if (c == "/"):
+                estado = "Z"
+            else:
+                estado = "B"
+    return cad2
 
 
 def tokeniza(cad):
@@ -85,3 +90,94 @@ def tokeniza(cad):
                 dentro = True
                 token = c
     return tokens
+
+
+cad = "var float x1;"\
+    "var float x2;"\
+    "/* coordenadas del 1er punto */"\
+    "var float y1;"\
+    "var float y2;"\
+    "/* coordenadas del 2do punto */"\
+    "var float m;"\
+    "/* pendiente de la recta */"\
+    "print(“escriba el valor de x1: “);"\
+    "read(x1);"\
+    "print(“escriba el valor de x2: “);"\
+    "read(x2);"\
+    "print(“escriba el valor de y1: “);"\
+    "read(y1);"\
+    "print(“escriba el valor de y2: “);"\
+    "read(y2);"\
+    "m = (y2 – y1) / (x2 – x1);"\
+    "print(“la pendiente es: “, m);" \
+    "end."
+
+
+print("programa Original")
+print(cad)
+cad2=quitaComentarios(cad)
+print("")
+print("Programa sin Comentarios")
+print(cad2)
+
+programa=cad2
+
+tokens = tokeniza(programa)
+#print(programa)
+estado = "z"
+variables = []
+for token in tokens:
+    if (estado == "z"):
+        if (token == "var"):
+            estado = "a"
+    elif (estado == "a"):
+        if esId(token):
+            estado = "b"
+            repetido = False
+            for v in variables:
+                if (v.nombre == token):
+                    repetido = True
+            if (repetido):
+                print("variable redeclarada!!")
+                print(token)
+                estado = "z"
+            else:
+                variables.append(Variable(token, "", "",""))
+
+    elif (estado == "b"):
+        if (token == ":"):
+            estado = "c"
+            #print("token:", token, "estado:", estado)
+        elif (token == ","):
+            estado = "a"
+    elif (estado == "c"):
+        if esTipo(token):
+            estado = "d"
+            #print("token:", token, "estado:", estado)
+            for v in variables:
+                if (v.tipo == ""):
+                    v.tipo = token
+    elif (estado == "d"):
+        if (token == ";"):
+            estado = "e"
+            #print("token:", token, "estado:", estado)
+    elif (estado == "e"):
+        if (token == "begin"):
+            estado = "z"
+            #print("token:", token, "estado:", estado)
+            
+        elif esId(token):
+            estado = "a"
+            repetido = False
+            for v in variables:
+                if (v.nombre == token):
+                    repetido = True
+            if (repetido):
+                print("variable redeclarada!!")
+                print(token)
+                estado = "z"
+            else:
+                variables.append(Variable(token, "", "", ""))
+
+for v in variables:
+    print(v.nombre, v.tipo, v.valor, v.direccion)
